e7142d951f338e505310250273c64db34211b9b0
LAB113 <LAB113@INFO.RN>
2021-04-28 09:57:36 +0200

    first

---
+++ Models/Comune.cs
@@ -0,0 +1,16 @@
+namespace Bartolini.Liam._4H.SaveRecord.Models
+{
+    public class Comune
+    {
+        public int ID { get; set; }
+        public string NomeComune { get; set; }
+        public string CodiceCatastale { get; set; }
+    
+        public Comune() { }
+
+        public Comune(string riga)
+        {
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.InteropServices;
+using System;
+
+namespace Bartolini.Liam._4H.SaveRecord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bartolini Liam 4H 28/04/2021");
+            
+        }
+    }
+}
